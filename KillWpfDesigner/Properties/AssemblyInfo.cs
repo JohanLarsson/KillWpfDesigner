d0b814cbe2f3b51d43ee1818849cbff9a9ab8bde
JohanLarsson <johan_gavle1@hotmail.com>
2019-06-11 09:40:57 +0200

    Use new csproj

--- KillWpfDesigner/Properties/AssemblyInfo.cs
+++ KillWpfDesigner/Properties/AssemblyInfo.cs
@@ -1,14 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("KillWpfDesigner")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Johan Larsson")]
-[assembly: AssemblyProduct("KillWpfDesigner")]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(false)]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
